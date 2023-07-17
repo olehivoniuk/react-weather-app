541f070f99d6b7784962cd8f9bc7743f1fa3b486
olehivoniuk <99249589+olehivoniuk@users.noreply.github.com>
2023-07-17 11:02:48 +0300

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    Weather App
+      <h1>Weather App</h1>
+   
+    <footer>
+      This pregect was coded by <a href="https://ornate-dodol-f50a44.netlify.app/">Bohdana Subotiak </a>{" "} and is
+    <a href="https://github.com/olehivoniuk/react-weather-app"
+    target="_blank">  open-sourced</a>
+    </footer>
     </div>
   );
 }
