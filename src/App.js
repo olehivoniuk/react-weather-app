7d0cf7673c113e78f1f26a6c9ba22a3dec3730d7
olehivoniuk <99249589+olehivoniuk@users.noreply.github.com>
2023-07-17 11:05:18 +0300

    changed link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <h1>Weather App</h1>
    
     <footer>
-      This pregect was coded by <a href="https://ornate-dodol-f50a44.netlify.app/">Bohdana Subotiak </a>{" "} and is
+      This pregect was coded by <a href="https://ornate-dodol-f50a44.netlify.app">Bohdana Subotiak </a>{" "} and is
     <a href="https://github.com/olehivoniuk/react-weather-app"
     target="_blank">  open-sourced</a>
     </footer>
