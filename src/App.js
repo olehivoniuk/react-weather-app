6b0c24a0cf6893bb2fef2393ddec2d018705e569
olehivoniuk <99249589+olehivoniuk@users.noreply.github.com>
2023-07-17 11:38:12 +0300

    added Bootstrap with through terminal

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
    
     <footer>
-      This pregect was coded by <a href="https://ornate-dodol-f50a44.netlify.app">Bohdana Subotiak </a>{" "} and is
+      This project  was coded by <a href="https://ornate-dodol-f50a44.netlify.app">Bohdana Subotiak </a>{" "} and is
     <a href="https://github.com/olehivoniuk/react-weather-app"
     target="_blank">  open-sourced</a>
     </footer>
+    </div>
     </div>
   );
 }
