563ff2fc6d71756c103ea2cc64f472a048cfdcc0
olehivoniuk <99249589+olehivoniuk@users.noreply.github.com>
2023-07-18 12:55:46 +0300

    Added API integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather/>
+      <Weather defaultCity= "Mew York"/>
    
     <footer>
       This project  was coded by <a href="https://ornate-dodol-f50a44.netlify.app">Bohdana Subotiak </a>{" "} and is
