05be343ff497805d441b2c71ce2a7f581bc73157
olehivoniuk <99249589+olehivoniuk@users.noreply.github.com>
2023-07-18 11:33:47 +0300

    Added basic styling CSS

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,10 @@ export default function Weather(){
             <form>
                 <div className="row">
                     <div className="col-9">
-                <input type="search" placeholder="Enter a city..." className="form-control" />
+                <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
                 </div>
                 <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary" />
+                <input type="submit" value="Search" className="btn btn-primary w-100" />
                 </div>
                 </div>
             </form>
@@ -19,16 +19,24 @@ export default function Weather(){
                 <li> Wednesday 07:00 </li>
                 <li>Mostly Cloudy </li>
             </ul> 
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                    <div className="row">
+                        <div className="col-3">
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
                     alt="Mostly Cloudy"
                     />
-                    6°C
+                        </div>
+                  
+                    <div className="col-3 ">
+                    <span className="temperature">6</span>
+                    <span className="unit">°C</span>
+                    </div>
+                    </div>
                 </div>
                 <div className="col-6">
                     <ul>
-                        <li>Precipitation: 15%;</li>
+                        <li>Precipitation: 15%</li>
                         <li> Humidity: 72% </li>
                         <li> Wind: 13 km/h</li>
                     </ul>
