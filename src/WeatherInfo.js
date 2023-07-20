7df03d626074de4aa5dcfe720815496bd768a4fe
olehivoniuk <99249589+olehivoniuk@users.noreply.github.com>
2023-07-20 10:51:14 +0300

    added relevant reactive icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 
 export default function WeatherInfo(props){
@@ -14,9 +15,8 @@ export default function WeatherInfo(props){
         <div className="col-6">
             <div className="row">
                 <div className="col-3">
-                <img src={props.data.iconUrl}
-            alt={props.data.description}
-            />
+                    <WeatherIcon code={props.data.icon} alt={props.data.description}/>
+                
                 </div>
           
             <div className="col-3 ">
