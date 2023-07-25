16309d0498ae033d9dd256ca38d8348ed87a00e5
olehivoniuk <99249589+olehivoniuk@users.noreply.github.com>
2023-07-25 12:34:36 +0300

    added useEffect to change the loaded data

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,45 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celcius");
-  const temperatureRef = React.useRef(null);
-
-  function convertToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature" ref={temperatureRef}>
-          {Math.round(props.celsius)}
-        </span>
-        <span className="unit" style={{ display: "inline" }}>°C
-          
-          <a href="/" onClick={convertToFahrenheit}>°F</a>
-        </span>
-      </div>
-    );
-  } else {
-    // Calculate temperature in Fahrenheit
-    const fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature" ref={temperatureRef}>
-          {Math.round(fahrenheit)}
-        </span>
-        <span className="unit" style={{ display: "inline" }}>°F
-          <a href="/" onClick={convertToCelsius}>°C</a> 
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">
+        °C 
+      </span>
+    </div>
+  );
 }
 
 
