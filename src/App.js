45b82c2e4432b7ff61ec4763cfea2908e7181b39
olehivoniuk <99249589+olehivoniuk@users.noreply.github.com>
2023-07-26 10:45:59 +0300

    updated city by default

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Weather defaultCity= "New York"/>
    
     <footer>
-      This project  was coded by <a href="https://ornate-dodol-f50a44.netlify.app" >Bohdana Subotiak </a>{" "} and is
+      This project  is
     <a href="https://github.com/olehivoniuk/react-weather-app" 
     target="_blank" rel="noreferrer">  open-sourced</a>
     </footer>
